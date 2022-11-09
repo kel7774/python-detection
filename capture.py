e77752381d65f2e7b5309a0cd290ff39063c7c7f
Kelli Landry <kellilandry@Kellis-MacBook-Pro.local>
2022-11-09 15:21:32 -0600

    face detection, motion detection basics

---
+++ capture.py
@@ -0,0 +1,21 @@
+import cv2, time
+
+video = cv2.VideoCapture(0)
+a = 0
+while True:
+    a = a + 1
+    check, frame = video.read()
+    print(check)
+    print(frame)
+
+    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("Capturing", grey)
+
+    key=cv2.waitKey(1)
+
+    if key == ord('q'):
+        break
+
+print(a)
+video.release()
+cv2.destroyAllWindows()
